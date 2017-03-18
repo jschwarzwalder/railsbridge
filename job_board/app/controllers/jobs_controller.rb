ba372b833aa2e88ba85ee1fccd1809e8f8b3c259
Jami <jamischwarzwalder@gmail.com>
2017-03-18 14:02:38 -0700

    processing update to edited job

--- job_board/app/controllers/jobs_controller.rb
+++ job_board/app/controllers/jobs_controller.rb
@@ -19,6 +19,12 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    redirect_to jobs_path
+  end
+
   private
 
   def job_params
