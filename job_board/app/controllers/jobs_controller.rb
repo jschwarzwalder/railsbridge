70b1eb79869fa54f3c08fd09d620074bb7e9de2e
Jami <jamischwarzwalder@gmail.com>
2017-03-18 15:09:01 -0700

    add delete function

--- job_board/app/controllers/jobs_controller.rb
+++ job_board/app/controllers/jobs_controller.rb
@@ -25,6 +25,12 @@ class JobsController < ApplicationController
     redirect_to jobs_path
   end
 
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
+
   private
 
   def job_params
